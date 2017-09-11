2134bc9028d6aaa78bd48edf49a2f9ded4bad0a0
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-09-11 18:01:56 +0200

    update run.sh

--- synda/run.sh
+++ synda/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --name synda --rm -it \
   -v $PWD/etc/mycreds.conf:/etc/synda/sdt/credentials.conf \
-  # -v data:/data \
-  # -v lib:/var/lib/synda \
+   -v data:/data \
+   -v lib:/var/lib/synda \
   synda /bin/bash
