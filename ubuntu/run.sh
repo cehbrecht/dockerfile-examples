bce360065e9b253789199d25bc30a6def181205d
Pingu Carsti <ehbrecht@dkrz.de>
2018-08-21 15:29:50 +0200

    update ubuntu

--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -3,10 +3,7 @@ docker run \
   --name ubuntudev \
   --rm -it \
   -p 5000:5000 \
-  -p 8080:8080 \
-  -p 8081:8081 \
-  -p 8443:8443 \
-  -v data:/data \
   -v ubuntudev:/opt \
   -v /Users/pingu/Documents/GitHub:/opt/src \
+  -v /Users/pingu/data:/opt/data \
   ubuntu/dev
