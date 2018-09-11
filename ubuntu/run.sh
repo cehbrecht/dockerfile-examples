98090a488445e9e8ad343084ffd6b843c9d983dd
Pingu Carsti <ehbrecht@dkrz.de>
2018-09-11 19:15:33 +0200

    added phoenix ports

--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -3,6 +3,8 @@ docker run \
   --name ubuntudev \
   --rm -it \
   -p 5000:5000 \
+  -p 8081:8081 \
+  -p 8443:8443 \
   -v ubuntudev:/opt \
   -v /Users/pingu/Documents/GitHub:/opt/src \
   -v /Users/pingu/data:/opt/data \
