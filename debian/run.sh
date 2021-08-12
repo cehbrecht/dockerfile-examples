ea20fbb984c98d1282f59e4ce97b96e05b772928
Carsten Ehbrecht <ehbrecht@dkrz.de>
2021-08-12 17:30:02 +0200

    update debian

--- debian/run.sh
+++ debian/run.sh
@@ -2,12 +2,5 @@
 docker run \
   --name debiandev \
   --rm -it \
-  -p 5000:5000 \
-  -p 8000:8000 \
-  -p 8081:8081 \
-  -p 8443:8443 \
-  -p 8090:8090 \
-  -p 8093:8093 \
-  -v debiandev:/opt \
   -v /Users/pingu/Documents/GitHub:/opt/src \
   debian/dev
